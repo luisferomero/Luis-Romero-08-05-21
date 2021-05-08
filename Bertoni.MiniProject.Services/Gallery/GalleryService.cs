23f1dfa89c53c24d1a08f81aecb66b20577ae67f
Luis Felipe Romero <rl.luisfe@gmail.com>
2021-05-08 12:43:07 -0400

    .

--- Bertoni.MiniProject.Services/Gallery/GalleryService.cs
+++ Bertoni.MiniProject.Services/Gallery/GalleryService.cs
@@ -23,8 +23,8 @@ namespace Bertoni.MiniProject.Services.Gallery
 
         public async Task<List<Album>> GetAllAlbums()
         {
-            var response = await httpClient.GetAsync("/albums");
-            var jsonResponse = await response.Content.ReadAsStringAsync();
+            HttpResponseMessage response = await httpClient.GetAsync("/albums");
+            string jsonResponse = await response.Content.ReadAsStringAsync();
             List<Album> albums = JsonConvert.DeserializeObject<List<Album>>(jsonResponse);
 
             return albums;
@@ -32,11 +32,11 @@ namespace Bertoni.MiniProject.Services.Gallery
 
         public async Task<List<Photo>> GetPhotosByAlbumId(int albumId)
         {
-            var responsePhotos = await httpClient.GetAsync("/photos");
-            var jsonResponsePhotos = await responsePhotos.Content.ReadAsStringAsync();
+            HttpResponseMessage responsePhotos = await httpClient.GetAsync("/photos");
+            string jsonResponsePhotos = await responsePhotos.Content.ReadAsStringAsync();
             List<Photo> photos = JsonConvert.DeserializeObject<List<Photo>>(jsonResponsePhotos).Where(photo => photo.AlbumId == albumId).ToList();
-            var responseComments = await httpClient.GetAsync("/comments");
-            var jsonResponseComments = await responseComments.Content.ReadAsStringAsync();
+            HttpResponseMessage responseComments = await httpClient.GetAsync("/comments");
+            string jsonResponseComments = await responseComments.Content.ReadAsStringAsync();
             List<Comment> comments = JsonConvert.DeserializeObject<List<Comment>>(jsonResponseComments).ToList();
 
             List<Photo> response = new List<Photo>();
@@ -51,8 +51,8 @@ namespace Bertoni.MiniProject.Services.Gallery
 
         public async Task<Photo> GetPhotoById(int photoId)
         {
-            var response = await httpClient.GetAsync("/photos");
-            var jsonResponse = await response.Content.ReadAsStringAsync();
+            HttpResponseMessage response = await httpClient.GetAsync("/photos");
+            string jsonResponse = await response.Content.ReadAsStringAsync();
             Photo photo = JsonConvert.DeserializeObject<List<Photo>>(jsonResponse).Where(photo => photo.Id == photoId).FirstOrDefault();
 
             return photo;
@@ -60,8 +60,8 @@ namespace Bertoni.MiniProject.Services.Gallery
 
         public async Task<List<Comment>> GetCommentsByPhotoId(int photoId)
         {
-            var response = await httpClient.GetAsync("/comments");
-            var jsonResponse = await response.Content.ReadAsStringAsync();
+            HttpResponseMessage response = await httpClient.GetAsync("/comments");
+            string jsonResponse = await response.Content.ReadAsStringAsync();
             List<Comment> comments = JsonConvert.DeserializeObject<List<Comment>>(jsonResponse).Where(comment => comment.PostId == photoId).ToList();
 
             return comments;
